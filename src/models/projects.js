4778de87a822bbaa74995153e51967e48b88f881
crashrosquist-crypto <crosquist@byupathway.edu>
2026-07-15 17:46:52 -0600

    more changes to routes and what not

--- src/models/projects.js
+++ src/models/projects.js
@@ -21,23 +21,63 @@ const getAllProjects = async() => {
 
 
 const getProjectsByOrganizationId = async (organizationId) => {
-      const query = `
-        SELECT
-          project_id,
-          organization_id,
-          title,
-          description,
-          location,
-          date
-        FROM project
-        WHERE organization_id = $1
-        ORDER BY date;
-      `;
-      
-      const queryParams = [organizationId];
-      const result = await db.query(query, queryParams);
+  const query = `
+    SELECT
+      service_project_id,
+      organization_id,
+      service_project_title AS title,
+      service_project_description AS description, -- Added prefix & aliased
+      service_project_location AS location,       -- Added prefix & aliased
+      service_project_date AS date                 -- Added prefix & aliased
+    FROM service_project
+    WHERE organization_id = $1
+    ORDER BY date;
+  `;
+  
+  const queryParams = [organizationId];
+  const result = await db.query(query, queryParams);
 
-      return result.rows;
+  return result.rows;
 };
 
-export { getAllProjects, getProjectsByOrganizationId }
+const getUpcomingProjects = async (number_of_projects) => {
+    const query = `
+    SELECT
+      service_project_id,
+      service_project_title AS title,
+      service_project_description AS description,
+      service_project_date AS date,
+      service_project_location AS location,
+      service_project.organization_id,
+      name AS organization_name
+    FROM service_project
+    INNER JOIN organization ON service_project.organization_id = organization.organization_id
+    ORDER BY service_project_date ASC
+    LIMIT $1;
+    `;
+
+    const result = await db.query(query, [number_of_projects]);
+    return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+    const query = `
+    SELECT
+      service_project_id,
+      service_project_title AS title,
+      service_project_description AS description,
+      service_project_date AS date,
+      service_project_location AS location,
+      service_project.organization_id,
+      name AS organization_name
+    FROM service_project
+    INNER JOIN organization ON service_project.organization_id = organization.organization_id
+    WHERE service_project_id = $1;
+    `;
+
+    const result = await db.query(query, [id]);
+    return result.rows[0];
+};
+
+
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails }
